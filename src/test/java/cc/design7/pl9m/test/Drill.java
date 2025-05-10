2cd64f2e0611361fc93a4223ffb5d3793a19d6e3
Chuigda Whitegive <icey@icey.tech>
2025-05-11 01:31:59 +0800

    Bind with to_string

--- src/test/java/cc/design7/pl9m/test/Drill.java
+++ src/test/java/cc/design7/pl9m/test/Drill.java
@@ -4,6 +4,7 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
+import java.util.Objects;
 
 public final class Drill {
     public static void main(String[] args) throws NoSuchMethodException, ClassNotFoundException, IllegalAccessException {
@@ -12,11 +13,10 @@ public final class Drill {
         Constructor<?> ctor = clazz.getConstructor(int.class);
         System.out.println(ctor);
 
-        // access System.getProperty
         MethodHandle handle = MethodHandles.lookup().findStatic(
-                System.class,
-                "getProperty",
-                MethodType.methodType(String.class, String.class)
+                Objects.class,
+                "toString",
+                MethodType.methodType(String.class, Object.class)
         );
         System.out.println(handle);
     }
