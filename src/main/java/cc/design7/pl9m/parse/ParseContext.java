736e2aac9cf306938ba775495b803f32c3c25779
Chuigda Whitegive <icey@icey.tech>
2025-05-10 16:06:21 +0800

    Add placeholder

--- src/main/java/cc/design7/pl9m/parse/ParseContext.java
+++ src/main/java/cc/design7/pl9m/parse/ParseContext.java
@@ -1,6 +1,27 @@
 package cc.design7.pl9m.parse;
 
 import cc.design7.pl9m.syntax.SourceLocation;
+import cc.design7.pl9m.util.Pair;
+import org.jetbrains.annotations.Nullable;
 
 public record ParseContext(int index, byte[] bytes, SourceLocation location) {
+    public Pair<Token, ParseContext> nextToken() {
+        return nextTokenImpl(
+                bytes,
+                index,
+                location.file(),
+                location.line(),
+                location.col()
+        );
+    }
+
+    public static Pair<Token, ParseContext> nextTokenImpl(
+            byte[] bytes,
+            int index,
+            @Nullable String file,
+            int line,
+            int col
+    ) {
+
+    }
 }
